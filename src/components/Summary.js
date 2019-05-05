d8d130c23d6406c08507b5d6afa39c8af6e79afe
Madhur Suria Mata Garcia <madhurmata@gmail.com>
2019-05-05 17:07:06 +0200

    save local storage done

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -6,21 +6,106 @@ import { statement } from '@babel/template';
 
 export default class Summary extends Component {
     state = {
+        numberOfNights: 1,
+        defaultPrice: 350,
+        booking: {
+            checkIn: "",
+            checkout: "",
+            adults: 0,
+            children: 0,
+            cardName: "",
+            cardPrice: 0,
+            totalPrice: 0,
+            numberOfNights: 0
+        }
+    };
+
+    componentDidMount = () => {
+       if(JSON.parse(localStorage.getItem("booking"))){
+            this.getInfo();  
+        }   
     }
 
-  render() {
-      const { summary, cardInfo } = this.props
-      const { checkIn, checkout, adults, children } = summary
-      const { cardName, cardPeople, cardPrice} = cardInfo
+
+    handleNumberOfNights = event => {
+        event.preventDefault();
+        this.setState({
+            numberOfNights: parseInt(event.target.value)
+        })
+    };
+
+   totalPrice = () => {
+        const { numberOfNights, defaultPrice, cardPrice } = this.state
+        const OneNightPrice = this.props.cardInfo.cardPrice
+        if(localStorage === undefined){
+            return "€" + (numberOfNights * cardPrice)
+        } else if (OneNightPrice) {
+            return "€" + numberOfNights * OneNightPrice
+        } else if(numberOfNights === 1){
+            return "€" + defaultPrice
+        }else{
+            const totalPrice = numberOfNights * defaultPrice
+            return "€" + totalPrice
+        }
+    }
+
+    handleSaveInfo = () => {
+        const {checkIn, checkout, adults, children} = this.props.summary;  
+        const {cardName, cardPrice } = this.props.cardInfo
+        const booking = {
+            checkIn,
+            checkout,
+            adults,
+            children,
+            cardName,
+            cardPrice: cardPrice || this.state.defaultPrice,
+            totalPrice: this.totalPrice(),
+            numberOfNights: parseInt(this.state.numberOfNights)
+        }
+        localStorage.setItem("booking", JSON.stringify(booking));
+        this.setState({
+            booking,
+        })
+    };
+
+    getInfo = () => {
+        const storedInfo = JSON.parse(localStorage.getItem("booking"))
+        const { checkIn, checkout, adults, children, cardName, totalPrice, numberOfNights} = storedInfo
+        this.setState({
+            checkIn,
+            checkout,
+            adults,
+            children,
+            cardName,
+            totalPrice,
+            numberOfNights
+        })
+    }
+
+    render() {
+        const {
+            numberOfNights,
+            cardName: cardNameLocalStorage,
+            checkIn: checkInLocalStorage,
+            checkout: checkoutLocalStorage,
+            adults: adultsLocalStorage,
+            children: childrenLocalStorage
+
+
+        } = this.state
+        const { summary, cardInfo } = this.props;
+        const { checkIn, checkout, adults, children } = summary;
+        const { cardName, cardPeople } = cardInfo;
     return (
       <div className="col-md-4 sidebar">
 
             <div className="card">
                 <h2>Reservation Summary</h2>
                 <div className="clearfix">
-                    <h5 className="pull-left">{ cardName ||"Mini Dreamy Room"}</h5>
+                    <h5 className="pull-left">{cardName || cardNameLocalStorage || "Mini Dreamy Room"}</h5>
                     <div className="form-group pull-right">
-                        <select className="pull-right" id="rooms">
+                        <select className="pull-right" id="rooms"  onChange={this.handleNumberOfNights}>
+                            <option value={numberOfNights} selected disabled hidden>{numberOfNights}</option>
                             <option>1</option>
                             <option>2</option>
                             <option>3</option>
@@ -48,18 +133,20 @@ export default class Summary extends Component {
 
                     <div className="card-content">
                         <p className="main">Reservation date</p>
-                        {checkIn && checkout ? 
-                            <p className="base">From <strong><span id="checkin-summary">{checkIn}</span></strong> to <strong id="checkout-summary">{checkout}</strong></p> :
+                        {(checkIn && checkout) || (checkoutLocalStorage && checkInLocalStorage) ? 
+                            <p className="base">From <strong><span id="checkin-summary">{checkIn || checkInLocalStorage}
+                            </span></strong> to <strong id="checkout-summary">{checkout || checkoutLocalStorage}</strong></p> :
                             <p className="summaryMessage">Place your Booking dates</p>
                         }
                     </div>
 
-                    {cardPeople ? <p className="main">{ cardPeople } People</p> :
+                    {adults || adultsLocalStorage ?  
                     <div className="card-content">
                         <p className="main">People</p>
-                        <p className="base" id="adults-summary">{adults || 2} Adults</p>
-                        <p className="base" id="children-summary">{children || 0} Children</p>
-                    </div>
+                        <p className="base" id="adults-summary">{adults || adultsLocalStorage || 2} Adults</p>
+                        <p className="base" id="children-summary">{children || childrenLocalStorage || 0} Children</p>
+                    </div> :
+                        <p className="main">{ cardPeople } People</p>
                     }
 
                     
@@ -69,13 +156,13 @@ export default class Summary extends Component {
                             <p className="base"><Link to="#">Price details ></Link></p>
                         </div>
                         <div className="right pull-right">
-                            <p className="main">{ cardPrice || "€350"}</p>
+                            <p className="main">{this.totalPrice()}</p>
                         </div>
                     </div>
 
-                    <Link to="#" className="btn btn-primary btn-group-justified">
+                    <button className="btn btn-primary btn-group-justified" onClick={this.handleSaveInfo}>
                         Save
-                    </Link>
+                    </button>
                 </div>
             </div>
 
